3576c31ce9e6308e580a73fe4e5086cbbb9384a4
Rishu2606 <rkmn8236@gmail.com>
2026-06-08 16:11:55 +0530

    auth: Implement AuthLaout

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Outlet } from 'react-router'
+import Header from '../pages/customer/shared/components/Header'
+import Footer from '../pages/customer/shared/components/Footer'
+
 
 const AuthLayout = () => {
   return (
+    <>
+    <Header/>
     <main className='min-min-vh-100'>
         <Outlet />
     </main>
+    <Footer/>
+    </>
   )
 }
 
