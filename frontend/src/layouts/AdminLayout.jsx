e3cc19db56f50590c0966063c5b8323b1f2fb8f6
Suman Mondal <sumanmondalmb71@gmail.com>
2026-06-08 21:48:00 +0530

    Admin: Fix admin sidebar on page scroll

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -2,12 +2,13 @@ import { Outlet } from "react-router";
 import Sidebar from '../pages/admin/shared/components/Sidebar';
 
 function AdminLayout() {
+  // TODO: authorization logic
+
   return (
-    <div className="d-flex min-vh-100 overflow-hidden bg-light">
+    <div className="d-flex vh-100 overflow-hidden bg-light">
       <Sidebar />
 
-      {/* Main Content Area */}
-      <main className="flex-fill overflow-auto w-100">
+      <main className="flex-fill h-100 overflow-y-auto w-100">
         <div className="px-3 py-2 px-md-4 py-md-3">
           {/* Spacer for mobile */}
           <div className="d-md-none pt-5"></div>
