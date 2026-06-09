cb52bc0b2c2e059d3b668115557232e56ad64f82
Suman Mondal <sumanmondalmb71@gmail.com>
2026-06-09 11:54:10 +0530

    Bug Fix: Import modules correctly

--- frontend/src/layouts/PublicLayout.jsx
+++ frontend/src/layouts/PublicLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet, Link } from "react-router";
 import Header from "../pages/public/shared/components/Header";
 import Footer from "../pages/public/shared/components/Footer";
-import CategoryTab from "../pages/public/shared/components/categories";
+import CategoryTab from "../pages/public/shared/components/Categories";
 
 import PublicHome from "../pages/public/public_home/pages/home_page";
 
