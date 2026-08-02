e1d2c4103f4470dbdaf127043478c21b8dacede9
sazidmugal <sazidmugal98@gmail.com>
2026-08-02 13:20:19 +0530

    remove api-gateway

--- backend/api-gateway/src/main/java/com/multivendor/apigateway/exception/MissingTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.multivendor.apigateway.exception;
-
-public class MissingTokenException extends JwtAuthenticationException {
-	public MissingTokenException(String message) {
-		super(message);
-	}
-}
